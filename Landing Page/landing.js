b7190b1d5cd26fdb20703dbb00bc63697ac08cf8
MADBravo <78062142+MADBravo@users.noreply.github.com>
2021-02-02 20:23:02 -0800

    Add files via upload

---
+++ Landing Page/landing.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('#showcase.button').on('click', function(e){
+    $('.item-list a').removeClass('active');
+    $(e.currentTarget).addClass('active');
+    });
+
+    if
+
+
+
+});
